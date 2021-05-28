357c2c1e3a970442d96a71358a387b864a8e7784
TonyAbrunhosa <37698049+TonyAbrunhosa@users.noreply.github.com>
2021-05-28 01:37:53 -0300

    Ajuste

--- Finances/FinancasDesktop/Classes/Convertions/Conversores.cs
+++ Finances/FinancasDesktop/Classes/Convertions/Conversores.cs
@@ -8,11 +8,6 @@ namespace FinancasDesktop.Classes.Convertions
 {
     public static class Conversores
     {
-        private static byte[] bIV =
-        { 0x50, 0x08, 0xF1, 0xDD, 0xDE, 0x3C, 0xF2, 0x18,
-        0x44, 0x74, 0x19, 0x2C, 0x53, 0x49, 0xAB, 0xBC };
-
-        private const string cryptoKey = "6403bd660841b8e5e4e9592228de8852";
         private static CultureInfo provider = new CultureInfo("pt-BR");
         public static int? ToIntOrNull(this string valor)
         {
@@ -153,68 +148,7 @@ namespace FinancasDesktop.Classes.Convertions
                 return null;
             }
         }
-        public static string Encrypt(string text)
-        {
-            try
-            {
-                if (!string.IsNullOrEmpty(text))
-                {
-                    byte[] bKey = Convert.FromBase64String(cryptoKey);
-                    byte[] bText = new UTF8Encoding().GetBytes(text);
-                    Rijndael rijndael = new RijndaelManaged();
-                    rijndael.KeySize = 256;
-                    MemoryStream mStream = new MemoryStream();
-                    CryptoStream encryptor = new CryptoStream(
-                        mStream,
-                        rijndael.CreateEncryptor(bKey, bIV),
-                        CryptoStreamMode.Write);
-                    encryptor.Write(bText, 0, bText.Length);
-                    encryptor.FlushFinalBlock();
-                    return Convert.ToBase64String(mStream.ToArray());
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception ex)
-            {
-                LogEx.GeraLogSimples("Erro ao criptografar\n" + ex.StackTrace);
-                return null;
-            }
-        }
-          
-        public static string Decrypt(string text)
-        {
-            try
-            {
-                if (!string.IsNullOrEmpty(text))
-                {
-                    byte[] bKey = Convert.FromBase64String(cryptoKey);
-                    byte[] bText = Convert.FromBase64String(text);
-                    Rijndael rijndael = new RijndaelManaged();
-                    rijndael.KeySize = 256;
-                    MemoryStream mStream = new MemoryStream();
-                    CryptoStream decryptor = new CryptoStream(
-                        mStream,
-                        rijndael.CreateDecryptor(bKey, bIV),
-                        CryptoStreamMode.Write);
-                    decryptor.Write(bText, 0, bText.Length);
-                    decryptor.FlushFinalBlock();
-                    UTF8Encoding utf8 = new UTF8Encoding();
-                    return utf8.GetString(mStream.ToArray());
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception ex)
-            {
-                LogEx.GeraLogSimples("Erro ao descriptografar\n" + ex.StackTrace);
-                return null;
-            }
-        }
+       
         public static DateTime? ToDateTimeOrNull(this object value)
         {
             if (value == null)
